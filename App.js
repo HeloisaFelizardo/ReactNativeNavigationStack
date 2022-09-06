6a2902b5509d5845c5fca9aa183573fd26f388b0
Heloisa Felizardo <helocmp@yahoo.com.br>
2022-09-06 19:44:48 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import TelaJavaScript from "./componentes/TelaJavaScript";
+import TelaReact from "./componentes/TelaReact";
+import TelaNode from "./componentes/TelaNode";
+
+const Stack = createStackNavigator();
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer >
+      <Stack.Navigator>
+        <Stack.Screen name="JavaScript" component = { TelaJavaScript } />
+        <Stack.Screen name="React" component = { TelaReact } />
+        <Stack.Screen name="NodeJS" component = { TelaNode } />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
